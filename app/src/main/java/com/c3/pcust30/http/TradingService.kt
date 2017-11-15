852387cecb62e3b8ba735726b67e7c3717e8188d
liyanjun <lyjmingan@gmail.com>
2017-11-15 22:59:48 +0800

    引入网络请求框架

--- app/src/main/java/com/c3/pcust30/http/TradingService.kt
+++ app/src/main/java/com/c3/pcust30/http/TradingService.kt
@@ -1,8 +1,19 @@
 package com.c3.pcust30.http
 
+import io.reactivex.Observable
+import retrofit2.http.POST
+import okhttp3.RequestBody
+import retrofit2.http.Body
+
+
 /**
  * 交易请求对象(用来请求后台数据)
  * Created by Lyan on 17/11/15.
  */
 interface TradingService {
+    /**
+     * 向后台发送JSON，后台返回JSON
+     */
+    @POST(TRADING_ADDRESS)
+    fun getResultJson(@Body requestBody: RequestBody): Observable<String>
 }
